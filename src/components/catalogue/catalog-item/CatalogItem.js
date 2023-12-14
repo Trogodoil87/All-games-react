457ce5998ae6743448366a40bf3088b234b7750f
Trogodoil87 <encho871337@gmail.com>
2023-12-15 00:47:35 +0200

    Changed display in catalog item

--- src/components/catalogue/catalog-item/CatalogItem.js
+++ src/components/catalogue/catalog-item/CatalogItem.js
@@ -4,7 +4,7 @@ export const CatalogItem = ({ game }) => {
     return (
         <div className="allGames">
             <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
+                <img src={game.imageUrl || "/images/avatar-1.jpg"} />
                 <h6>{game?.category}</h6>
                 <h2>{game?.title}</h2>
                 <Link to={`/details/${game?._id}`} className="details-button">Details</Link>
--- src/components/edit/Edit.js
+++ src/components/edit/Edit.js
@@ -1,21 +1,43 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import * as gameService from "../services/gameService";
+
 export const Edit = () => {
+    const [game, setGame] = useState({});
+    const { gameId } = useParams();
+
+    useEffect(() => {
+        gameService.getById(gameId)
+            .then(result => {
+                setGame(result);
+            });
+    }, [gameId]);
+
+    function onSubmitClickHandler(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        
+
+    }
+
     return (
         <section id="edit-page" className="auth">
-            <form id="edit">
+            <form id="edit" onSubmit={onSubmitClickHandler}>
                 <div className="container">
 
                     <h1>Edit Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
-                    <input type="text" id="title" name="title" value="" />
+                    <input type="text" id="title" name="title" value={game.title} />
 
                     <label htmlFor="category">Category:</label>
-                    <input type="text" id="category" name="category" value="" />
+                    <input type="text" id="category" name="category" value={game.category} />
 
                     <label htmlFor="levels">MaxLevel:</label>
-                    <input type="number" id="maxLevel" name="maxLevel" min="1" value="" />
+                    <input type="number" id="maxLevel" name="maxLevel" min="1" value={game.maxLevel} />
 
                     <label htmlFor="game-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" value="" />
+                    <input type="text" id="imageUrl" name="imageUrl" value={game.imageUrl} />
 
                     <label htmlFor="summary">Summary:</label>
                     <textarea name="summary" id="summary"></textarea>
