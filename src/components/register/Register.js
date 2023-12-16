8aa32627a13bd8f10bb15d6b2679e8c04c2b0393
Trogodoil87 <encho871337@gmail.com>
2023-12-16 18:40:53 +0200

    Add Authentication

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,9 +1,36 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import * as authService from "../services/authServices";
+import { AuthContext } from "../../contexts/AuthContext";
+
 
 export const Register = () => {
+    const { userLogin } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    function onSubmit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const email = formData.get("email");
+        const password = formData.get("password");
+        const confirmPassword = formData.get("confirm-password");
+
+        if (password !== confirmPassword) {
+            return;
+        }
+
+        authService.register(email, password)
+            .then(authData => {
+                userLogin(authData);
+                navigate('/');
+            })
+    }
+
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form onSubmit={onSubmit} id="register">
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
